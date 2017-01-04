f11195980356b80f070b737015f3a907e14acd06
Banhawy <the.benhawy@gmail.com>
2017-01-03 19:09:57 -0600

    Early testing with API call, firts commit before deploying to github

--- js/API.js
+++ js/API.js
@@ -4,13 +4,23 @@
   });
 */
 
-$("#quote_btn").on("click", function() {
+
+
+/*$("#quote_btn").on("click", function() {
 
   $.ajax({
     type: 'GET',
     url: 'api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=9d1f664d7c8d2bf96dae8ae0cb28164e ',
-    dataType: 'json',
+    dataType: 'jsonp',
     success: function(data) {
-    	var main = data[wather].main;
-    	$(#location).html(main);
-    }})});
+    	var main = data.wather.main;
+        $("#location").html("<h1>It Works!</h1>");
+    }})});*/
+
+
+$(document).ready(function () {
+    $.get('api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=9d1f664d7c8d2bf96dae8ae0cb28164e', function (data) {
+        $("#location").html("<h1>It Works!</h1>");
+
+    });
+});
