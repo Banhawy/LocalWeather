65829ae0282d7e771f20956719f8e65153287ced
Banhawy <the.benhawy@gmail.com>
2017-01-07 16:56:06 -0600

    Fixed API calls, added styling and textbox city search

--- js/API.js
+++ js/API.js
@@ -1,26 +1,68 @@
-//API Call
-/*navigator.geolocation.getCurrentPosition(function (position){
-    $("#location").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
-  });
-*/
+$(document).ready(function () { 
 
+        var apiWeather;
 
+        //Weather API call. Checks weather given location. Sets Temperature
+        function getWeather(apiLink){
+            $.getJSON(apiLink, function(data) {
+                var location = data.name + ', '+ data.sys.country;
+                var celsiusTemp = Math.floor(data.main.temp);
+                var fahrenheitTemp = Math.floor(celsiusTemp*9/5 +32);
+                var weatherRegex = data.weather[0].description;
+                var weather = weatherRegex.replace(/\s/, "%20");
+                var weatherID = data.weather[0].id;
+                var apiFlickr = "https://api.flickr.com/services/rest/?method=flickr.photos.search&text=" + weather + "&format=json&nojsoncallback=1&api_key=ec0ca2ee160a19f017eb3ddcbda7ccb4";
+                var weatherIcon = "<i class='wi wi-owm-"+ weatherID +"'></i>";
 
-$("#quote_btn").on("click", function() {
+                //Display weather on screen
+                $("#location").html("Looks like " + weatherRegex + " in " + location);
+                $("#temperature").html(celsiusTemp + " °C");
+                $("#weather-icon").html(weatherIcon);
 
-  $.ajax({
-    type: 'GET',
-    url: 'api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=9d1f664d7c8d2bf96dae8ae0cb28164e ',
-    dataType: 'jsonp',
-    success: function(data) {
-    	var main = data.wather.main;
-        $("#location").html("<h1>It Works!</h1>");
-    }})});
+                //Choose Celsius or Fahrenheit
+                $("#fahrenheit").on('click',  function() {
+                    $("#temperature").html(fahrenheitTemp + " °F");
+                });
+                $("#celsius").on('click',  function() {
+                    $("#temperature").html(celsiusTemp + " °C");
+                });
 
+                //Flickr API call. Gets random weather related photo, given weather at location
+                $.get(apiFlickr, function(myresult){
+                    var random = Math.floor(Math.random() * 100)
+                    var id = myresult.photos.photo[random].id;
+                    var photoSizeApi = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&photo_id=" + id + "&format=json&nojsoncallback=1&api_key=ec0ca2ee160a19f017eb3ddcbda7ccb4";
 
-/*$(document).ready(function () {
-    $.get('api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=9d1f664d7c8d2bf96dae8ae0cb28164e', function (data) {
-        $("#location").html("<h1>It Works!</h1>");
+                    //Flickr API imagesize. Gets chosen images' large sizes, and sets background image
+                    $.getJSON( photoSizeApi , function(image) {
+                        var allSizes = image.sizes.size; //array
+                        var bigSizeUrl;
+                        for (var i = 0; i<allSizes.length; i++){
+                            if (allSizes[i].label === "Original" || allSizes[i].label === "Large" ){
+                                bigsizeUrl = allSizes[i].source;
+                            }
+                        }
+                            $("body").css("background-image", 'url('+ bigsizeUrl +')');
+                    });
+                });
+            });
+        }
+    
+        navigator.geolocation.getCurrentPosition(function (position){
+                var lat = position.coords.latitude;
+                var long = position.coords.longitude;
+                apiWeather = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat +"&lon="+ long +"&units=metric&APPID=9d1f664d7c8d2bf96dae8ae0cb28164e";
+                getWeather(apiWeather);
+            });
 
-    });
-});*/
+            //In case manual city search 
+            $("input[type='text']").keypress(function(e){
+                if(e.which === 13){
+                  var city = $(this).val();
+                  apiWeather = "http://api.openweathermap.org/data/2.5/weather?q="+ city +" &units=metric&APPID=9d1f664d7c8d2bf96dae8ae0cb28164e";
+                  $(this).val("");
+                  getWeather(apiWeather);
+                }
+            });
+});
+
